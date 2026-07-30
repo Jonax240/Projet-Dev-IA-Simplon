97e6e45a11260ba64d7a91bce88374d7e28a470a
Jonathan <154245216+Jonax240@users.noreply.github.com>
2026-07-30 14:57:56 +0000

    Livrables terminés du brief projet

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
 
-données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
+df = pd.read_csv("ventes.csv")
 
-figure.write_html('ventes-par-region.html')
 
-print('ventes-par-région.html généré avec succès !')
+df.columns = ["date", "produit", "prix", "qte", "region"]
+
+
+df["ca"] = df["prix"] * df["qte"]
+
+
+df_ventes_produit = (
+    df.groupby("produit", as_index=False)["qte"].sum().rename(columns={"qte": "total_qte"})
+)
+
+fig1 = px.bar(
+    df_ventes_produit,
+    x="produit",
+    y="total_qte",
+    title="Total des ventes par produit (Quantités)",
+    labels={"produit": "Produit", "total_qte": "Quantité totale vendue"},
+)
+
+fig1.write_html("ventes-par-produit.html")
+
+
+df_ca_produit = (
+    df.groupby("produit", as_index=False)["ca"].sum().rename(columns={"ca": "total_ca"})
+)
+
+fig2 = px.bar(
+    df_ca_produit,
+    x="produit",
+    y="total_ca",
+    title="Chiffre d'affaires par produit",
+    labels={"produit": "Produit", "total_ca": "Chiffre d'affaires (€)"},
+)
+
+fig2.write_html("chiffre-affaires-par-produit.html")
+
+print("Graphiques générés avec succès !")
